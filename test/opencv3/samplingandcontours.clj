4cce567bde6d2f038fd7e3729feed8208e0d7eb0
Nicolas Modrzyk <hellonico@gmail.com>
2017-07-25 15:18:23 +0900

    sampling and contours

--- test/opencv3/samplingandcontours.clj
+++ test/opencv3/samplingandcontours.clj
@@ -10,8 +10,17 @@
   (->
     img
     (clone)
-    (cvt-color! COLOR_BGR2GRAY)
-    (median-blur! 13)))
+    (cvt-color! COLOR_BGR2GRAY)))
+
+; erode and dilate
+(def k 8)
+(dotimes[ i k]
+  (-> gray
+    (median-blur! 17)
+    (erode! (new-mat))
+    (median-blur! 17)
+    (dilate! (new-mat))))
+
 (def contours (new-arraylist))
 (find-contours gray contours (new-mat) RETR_CCOMP CHAIN_APPROX_SIMPLE)
 
@@ -20,16 +29,23 @@
 (draw-contours conv contours 0 (new-scalar 255 0 0) 3)
 
 ; draw an ellipse in the bouded box
-(def boxed (clone img))
+(def boxed
+  (clone img))
 (def b-box
   (bounding-rect (first contours)))
 (def center
   (u/middle-of-two-points (.br b-box) (.tl b-box)))
 (def r-rect
   (new-rotatedrect center (.size b-box) 1 ))
-(ellipse boxed r-rect (new-scalar 255 0 0) 10)
-(u/show boxed)
+(ellipse boxed r-rect (new-scalar 255 0 0) 3)
 
+; display results
 (def results (new-mat))
+(defn annotate[mat text]
+ (put-text mat
+  text (new-point 100 100) FONT_HERSHEY_PLAIN 1 (new-scalar 255 0 0) 1))
+(annotate img "original")
+(annotate conv (str "blurred (" k")"))
+(annotate boxed "ellipse")
 (vconcat [img conv boxed] results)
 (u/show results)
