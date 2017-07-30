f2574d637c56b8a9d0cf08e88e229c08aaa891d2
Nicolas Modrzyk <hellonico@gmail.com>
2017-07-30 09:39:09 +0900

    update filter

--- test/opencv3/video/filter.clj
+++ test/opencv3/video/filter.clj
@@ -4,40 +4,31 @@
     [opencv3.video :refer :all]
     [opencv3.utils :as u]))
 
-(def height 300)
-(def width 400)
+(def lower-red  (new-scalar 0 70 70))
+(def upper-red (new-scalar 10 255 255))
 
-(def capture (new-videocapture))
-(doto capture
-  (.open 0)
-  (.set CAP_PROP_FRAME_WIDTH width)
-  (.set CAP_PROP_FRAME_HEIGHT height))
+(defn filter-stream[ lower upper buffer ]
+   (u/resize-by buffer 0.5)
+   (let [
+     hsv (-> buffer clone (cvt-color! COLOR_BGR2HSV))
+     mask (new-mat)
+     res (new-mat)
+     output (new-mat)
+     ]
+     (in-range hsv lower-red upper-red mask)
+     (bitwise-and! buffer res mask)
+     (hconcat [ buffer hsv (cvt-color! mask COLOR_GRAY2RGB) res] output)
+     output))
 
-(def window
-  (u/show (new-mat height width CV_8UC3 (new-scalar 255 255 255))))
-
-(defn filter-stream[ seconds ]
- (let [  timeout seconds ; seconds
-         start (System/currentTimeMillis)
-         ellapsed (atom 0)
-         buffer (new-mat)
-         lower-red  (new-scalar 0 70 70)
-         upper-red (new-scalar 10 255 255)]
-     (while (< @ellapsed timeout)
-       (reset! ellapsed (/ (- (System/currentTimeMillis) start) 1000))
-       (.read capture buffer)
-       (u/resize-by buffer 0.5)
-       (let [
-         hsv (-> buffer clone (cvt-color! COLOR_BGR2HSV))
-         mask (new-mat)
-         res (new-mat)
-         output (new-mat)
-         ]
-         (in-range hsv lower-red upper-red mask)
-         (bitwise-and! buffer res mask)
-         (hconcat [ buffer hsv (cvt-color! mask COLOR_GRAY2RGB) res] output)
-         (u/re-show window output)))))
+(defn -main[& args]
+   (u/simple-cam-window
+     (partial filter-stream lower-red upper-red )))
 
 (comment
-  (filter-stream 10)
+  (-main)
+
+  (u/simple-cam-window
+     (partial filter-stream
+        (new-scalar 100 70 70)
+        (new-scalar 110 255 255)))
   )
