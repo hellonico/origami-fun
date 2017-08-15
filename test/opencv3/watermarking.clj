6ce6f7ee577ec87f983282802fcd421f8bd0706c
Nico <hellonico@gmail.com>
2017-08-15 17:47:22 +0900

    submat has been added to core .. updating name

--- test/opencv3/watermarking.clj
+++ test/opencv3/watermarking.clj
@@ -7,8 +7,8 @@
 (def mark (-> "resources/images/nicologo.gif" imread (u/resize-by 0.5)))
 ; (def submat (.submat source (.rows mark) (* 2 (.rows mark)) (.cols mark) (* 2 (.cols mark) )))
 (println mark)
-(def submat (.submat source (new-rect 50 50 (.cols mark) (.rows mark) )))
-(.copyTo mark submat)
+(def subma-t (.submat source (new-rect 50 50 (.cols mark) (.rows mark) )))
+(.copyTo mark subma-t)
 
 (u/show source
   {:frame {:width 600}}
