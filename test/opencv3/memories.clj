20d9d9d2175e49aca5c7f9dea8470b20c0a17b9a
Nicolas Modrzyk <hellonico@gmail.com>
2017-08-19 22:28:18 +0900

    added performance test

--- test/opencv3/memories.clj
+++ test/opencv3/memories.clj
@@ -3,9 +3,19 @@
    [opencv3.utils :as u]
    [opencv3.core :refer :all]))
 
+;
+; basic mat performance test.
+;
+
+; COUNT | TIME          |  machine  | use clone
+; 500   | 24410.345098  |  macbook  | no
+; 500   | 32727.560441  |  macbook  | yes
+; 100   | 1313.304997   |  macbook  | no
+; 100   | 1466.568298   |  macbook  | yes
+
 (time
 (do
-(def value 1000)
+(def value 100)
 ; (double (/ 1 value))
 (def one
   (->
@@ -17,12 +27,16 @@
 (def target
   (new-mat (* 12 value) (* 16 value) CV_8UC3))
 
-(doseq [ j (range 0 (dec value))]
- (doseq [ i (range 0 (dec value))]
+(dotimes [ j value ]
+ (dotimes [ i value]
   (let [ s (.submat target (new-rect (* 16 i) (* 12 j) 16 12)) ]
-    (.copyTo (-> one clone) s))))
+    (.copyTo (-> one clone) s))
+    ; (.copyTo one s))
+    ))
 
 (u/show
-  (-> target (u/resize-by 0.1))
+  (-> target (u/resize-by 1))
   {:frame {:width 1600 :height 1200 }}
-  )))
+  )
+
+  ))
