1cf0e8763f6281e7e8ae103a3253d13fec6e2f28
Nicolas Modrzyk <hellonico@gmail.com>
2017-08-23 17:48:34 +0900

    documenting

--- scripts/topdf.sh
+++ scripts/topdf.sh
@@ -5,11 +5,24 @@ function convertone() {
 	extension="${filename##*.}"
 	base="${filename%.*}"
 
+  echo "Deleting previous file";
+  rm -f ./pdf/$base.pdf
+
 	$DIR/url2pdf --print-orientation Portrait --print-paginate YES --enable-javascript YES --url \
   "http://localhost:10000/worksheet.html?filename=gorillas/$filename" \
   --autosave-name $base.pdf --autosave-path ./pdf
 }
 
-for i in `ls gorillas` ; do
-  convertone $i
-done
+function convertall() {
+  for i in `ls gorillas` ; do
+    convertone $i
+  done
+}
+
+if [ -z ${1+x} ]; then
+  echo "Generating all pdfs";
+  convertall
+else
+  echo "Generating one pdf: '$1'";
+  convertone $1
+fi
