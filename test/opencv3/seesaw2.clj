4e217e1c5a965434ab6e420978c6d2314fe4ae2c
Nicolas Modrzyk <hellonico@gmail.com>
2017-08-23 09:37:25 +0900

    small control window

---
+++ test/opencv3/seesaw2.clj
@@ -0,0 +1,54 @@
+(ns opencv3.seesaw2
+(:require
+  [opencv3.core :refer :all]
+  [opencv3.utils :as u])
+(:use seesaw.core))
+
+(def state
+  (atom {
+    :img  (-> "resources/nico.jpg" (imread 0))
+    :bitwise false
+    :a 0
+    :b 300}))
+
+(def frame
+(->
+(frame
+:title "Small OpenCV Pannel"
+:on-close :dispose
+:content
+ (vertical-panel
+  :items [
+  (label :text "inverse")
+  (checkbox :id :c :text "Inverse"
+  :listen [:state-changed
+   (fn [_]
+   (swap! state assoc-in [:bitwise] (not (@state :bitwise))))])
+
+  (label :text "Threshold Parameter 1")
+  (slider :min 0 :max 200
+     :listen [:state-changed
+     (fn [e] (let [v (-> e (.getSource) (.getValue))]
+     (swap! state assoc-in [:a] v)))])
+  (label :text "Threshold Parameter 2")
+  (slider :min 0 :max 200
+     :listen [:state-changed
+     (fn [e] (let [v (-> e (.getSource) (.getValue))]
+     (swap! state assoc-in [:b] v)))])]))
+ pack!
+ show!))
+
+(defn redraw [ new-state ]
+  ; (clojure.pprint/pprint new-state)
+  (->
+    (new-state :img)
+    (clone)
+    (threshold! (new-state :a) (new-state :b) 1)
+    ((fn [x] (if (true? (new-state :bitwise)) (bitwise-not! x) x)))
+    (imwrite "output/nicosaw.png")))
+
+(add-watch state :state (fn [ _ _ _ new-state]
+  (redraw new-state)))
+
+(remove-watch state :state)
+; (println @state)
--- test/opencv3/watermarking.clj
+++ test/opencv3/watermarking.clj
@@ -5,11 +5,8 @@
 
 (def source (-> "resources/images/kidrunning.jpeg" imread (u/resize-by 0.5)))
 (def mark (-> "resources/images/nicologo.gif" imread (u/resize-by 0.5)))
-; (def submat (.submat source (.rows mark) (* 2 (.rows mark)) (.cols mark) (* 2 (.cols mark) )))
-(println mark)
-(def subma-t (.submat source (new-rect 50 50 (.cols mark) (.rows mark) )))
-(.copyTo mark subma-t)
 
-(u/show source
-  {:frame {:width 600}}
-  )
+(def subma-t (submat source (new-rect 50 50 (.cols mark) (.rows mark) )))
+(copy-to mark subma-t)
+
+(u/show source {:frame {:width 600}})
