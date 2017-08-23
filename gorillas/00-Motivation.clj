6597e11bc4369a526587204bee2fcc21ea3f389a
Nicolas Modrzyk <hellonico@gmail.com>
2017-08-23 23:48:53 +0900

    tennis ball and more documentation

---
+++ gorillas/00-Motivation.clj
@@ -0,0 +1,14 @@
+;; gorilla-repl.fileformat = 1
+
+;; **
+;;; # Motivation
+;;; 
+;;; OpenCV is quite well known in the domain of image manipulation. 
+;;; It has all those secret boots you can wear and use to apply filters, resize, rotate, find shapes, remove background etc ...
+;;; 
+;;; In those days of neural networks, image pre-analysis is actual a fundamental of preparing a training set.
+;; **
+
+;; @@
+
+;; @@
