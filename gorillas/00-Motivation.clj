b0672cf5e60009c8c4d72ea148861249dd569dfb
Nicolas Modrzyk <hellonico@gmail.com>
2017-08-24 17:36:52 +0900

    lagging

--- gorillas/00-Motivation.clj
+++ gorillas/00-Motivation.clj
@@ -1,12 +1,16 @@
 ;; gorilla-repl.fileformat = 1
 
 ;; **
-;;; # Motivation
+;;; # OpenCV meets Clojure: Motivation
 ;;; 
 ;;; OpenCV is quite well known in the domain of image manipulation. 
 ;;; It has all those secret boots you can wear and use to apply filters, resize, rotate, find shapes, remove background etc ...
 ;;; 
-;;; In those days of neural networks, image pre-analysis is actual a fundamental of preparing a training set.
+;;; In those days of neural networks, image pre-analysis is actual a fundamental step in preparing a training set.
+;;; 
+;;; While OpenCV is fantastic to achieve all those tasks, its also quite a pain to re-compile and make the install manually on each machine you want to run it, and I do feel this is a gigantic wall for most of the people out there who just want to use it.
+;;; 
+;;; 
 ;; **
 
 ;; @@
