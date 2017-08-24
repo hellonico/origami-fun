98d4400ce978e43a6e36051be803a5e0e39889f0
Nicolas Modrzyk <hellonico@gmail.com>
2017-08-24 11:07:49 +0900

    starting booking ; tooling in place

--- scripts/topdf.sh
+++ scripts/topdf.sh
@@ -1,28 +1,36 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function convertone() {
+  echo "Generating pdf: '$1'";
+
 	filename=$1
 	extension="${filename##*.}"
 	base="${filename%.*}"
 
-  echo "Deleting previous file";
+  # echo "Deleting previous file";
   rm -f ./pdf/$base.pdf
 
 	$DIR/url2pdf --print-orientation Portrait --print-paginate YES --enable-javascript YES --url \
   "http://localhost:10000/worksheet.html?filename=gorillas/$filename" \
-  --autosave-name $base.pdf --autosave-path ./pdf
+  --autosave-name $base.pdf --autosave-path ./pdf > /dev/null 2>&1
 }
 
 function convertall() {
   for i in `ls gorillas` ; do
     convertone $i
   done
+
+}
+
+function book() {
+  rm book.pdf
+  java -jar ~/Downloads/pdfbox-app-2.0.7.jar PDFMerger pdf/*.pdf book.pdf
 }
 
 if [ -z ${1+x} ]; then
-  echo "Generating all pdfs";
   convertall
+  book
 else
-  echo "Generating one pdf: '$1'";
   convertone $1
+  book
 fi
