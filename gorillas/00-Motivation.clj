bcd762d4f2c35895c0b1b94f07a7c6bc2ed109c4
Nicolas Modrzyk <hellonico@gmail.com>
2017-09-11 12:02:32 +0900

    playing with mats

--- gorillas/00-Motivation.clj
+++ gorillas/00-Motivation.clj
@@ -1,16 +1,18 @@
 ;; gorilla-repl.fileformat = 1
 
 ;; **
-;;; # OpenCV meets Clojure: Motivation
+;;; # OpenCV meets Clojure
+;;; 
+;;; ## Motivations
 ;;; 
 ;;; OpenCV is quite well known in the domain of image manipulation. 
 ;;; It has all those secret boots you can wear and use to apply filters, resize, rotate, find shapes, remove background etc ...
 ;;; 
 ;;; In those days of neural networks, image pre-analysis is actual a fundamental step in preparing a training set.
 ;;; 
-;;; While OpenCV is fantastic to achieve all those tasks, its also quite a pain to re-compile and make the install manually on each machine you want to run it, and I do feel this is a gigantic wall for most of the people out there who just want to use it.
+;;; While OpenCV is fantastic to achieve all those tasks, its also quite a pain to re-compile and make the install manually on each machine you want to run it. 
 ;;; 
-;;; 
+;;; This book presents Origami, a simple Clojure wrapper auto generated from the java port of OpenCV.
 ;; **
 
 ;; @@
