1b9027630f14fdc48e951f8d021095a6fb7dac71
Nicolas Modrzyk <hellonico@gmail.com>
2017-10-23 16:01:19 +0900

    scaling

--- scala-fun/scala/SimpleOpenCV.scala
+++ scala-fun/scala/SimpleOpenCV.scala
@@ -3,11 +3,11 @@ import org.opencv.core.CvType._
 import clojure.lang.RT.loadLibrary
 
 object SimpleOpenCV {
- 	loadLibrary(Core.NATIVE_LIBRARY_NAME)
+ 	  loadLibrary(Core.NATIVE_LIBRARY_NAME)
 
     def main(args: Array[String]) {
     	val mat = Mat.eye(3, 3, CV_8UC1)
-        println("mat = " + mat.dump())
+      println("mat = " + mat.dump())
     }
 
 }
