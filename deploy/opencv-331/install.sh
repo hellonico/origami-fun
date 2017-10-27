d77e73a301cf1f6ba67f76a43f81ed9544286a34
Nicolas Modrzyk <hellonico@gmail.com>
2017-10-27 13:52:56 +0900

    3.3.1 installers

---
+++ deploy/opencv-331/install.sh
@@ -0,0 +1,13 @@
+mvn install:install-file \
+-DgroupId=opencv \
+-DartifactId=opencv \
+-Dversion=3.3.1 \
+-Dpackaging=jar \
+-Dfile=opencv-331.jar
+
+mvn install:install-file \
+-DgroupId=opencv \
+-DartifactId=opencv-native \
+-Dversion=3.3.1 \
+-Dpackaging=jar \
+-Dfile=opencv-native.jar
