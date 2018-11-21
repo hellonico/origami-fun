6a9978b7b35889ea0227b49fa29e57417180c645
Nicolas Modrzyk <hellonico@gmail.com>
2018-11-21 15:00:37 +0900

    Non-Photorealistic Rendering

--- project.clj
+++ project.clj
@@ -1,13 +1,10 @@
 (defproject origami-fun "0.1.0-SNAPSHOT"
   :plugins [[hellonico/lein-gorilla "0.4.2"]]
   :test-paths ["test" "samples"]
-  :resource-paths ["rsc"]
   :gorilla-options {:load-scan-exclude #{".git" ".svn" "src" "test" "project.clj"}}
   :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
-  :aliases {
-            "notebook" ["gorilla" ":ip" "0.0.0.0" ":port" "10000"]
-            "cv_ok" ["run" "-m" "opencv4.ok"]
-            }
+  :aliases {"notebook" ["gorilla" ":ip" "0.0.0.0" ":port" "10000"]
+            "cv_ok" ["run" "-m" "opencv4.ok"]}
   :profiles {:dev {
     :resource-paths ["resources"]
     :dependencies [
@@ -17,5 +14,4 @@
   :dependencies [
    [org.clojure/clojure "1.9.0"]
    [org.clojure/tools.cli "0.3.5"]
-   [origami "4.0.0-beta7"]
-])
+   [origami "4.0.0-beta8"]])
