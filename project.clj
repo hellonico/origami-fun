04b73f09f842551b6f628c62bbff4349da64bd14
Nicolas Modrzyk <hellonico@gmail.com>
2019-03-08 10:53:01 +0900

    manga gamma etc..

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject origami-fun "0.1.0-SNAPSHOT"
   :plugins [[hellonico/lein-gorilla "0.4.2"][lein-jupyter "0.1.16"]]
   :test-paths ["test" "samples"]
-  :gorilla-options {:load-scan-exclude #{".git" ".svn" "src" "test" "project.clj"}}
+  :gorilla-options {:load-scan-exclude #{".git" "project.clj" ".svn" "samples" "src" "test"}}
   :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
   :aliases {"notebook" ["gorilla" ":ip" "0.0.0.0" ":port" "10000"]
             "cv_ok" ["run" "-m" "opencv4.ok"]}
@@ -12,4 +12,4 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [seesaw "1.4.5"]
-                 [origami "4.0.0-6"]])
+                 [origami "4.0.0-7"]])
