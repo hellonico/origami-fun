3ae4b03678078b9972bf3154b5ccb18b8767a52c
Nicolas Modrzyk <hellonico@gmail.com>
2020-04-15 12:16:41 +0900

    new jupyter docs

--- project.clj
+++ project.clj
@@ -12,5 +12,5 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [seesaw "1.4.5"]
-                 [origami "4.2.0-0"]
-                 [origami/filters "1.0"]])
+                 [origami "4.3.0-1"]
+                 [origami/filters "1.4"]])
