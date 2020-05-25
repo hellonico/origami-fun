d38131de7d5861c10cb217e4b1c43ce6da43c56e
Nicolas Modrzyk <hellonico@gmail.com>
2020-05-25 14:52:48 +0900

    merge

--- project.clj
+++ project.clj
@@ -2,20 +2,17 @@
   :test-paths ["test" "samples"]
   :gorilla-options {:load-scan-exclude #{".git" "project.clj" ".svn" "samples" "src" "test"}}
   :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
-  :aliases {
-    "jnotebook" ["with-profile" "jupyter" "jupyter" "notebook"]
-    "notebook" ["with-profile" "gorilla" "gorilla" ":ip" "0.0.0.0" ":port" "10000"]
+  :aliases {"jnotebook" ["jupyter" "notebook"]
+            "notebook" ["with-profile" "-dev" "gorilla" "gorilla" ":ip" "0.0.0.0" ":port" "10000"]
             "cv_ok" ["run" "-m" "opencv4.ok"]}
-  :profiles {:dev {
-    :dependencies []}
-  :jupyter {
-    :dependencies [ [clojupyter "0.3.1"]]
-    :plugins [[hellonico/lein-jupyter "0.1.17"]]}
-  :gorilla {
-    :dependencies [[hellonico/gorilla-repl "0.4.1"]]
-    :plugins [[hellonico/lein-gorilla "0.4.2"]]
-  }
-  }
+  :profiles {:dev {:dependencies [[clojupyter "0.3.1"]]
+                   :plugins [[hellonico/lein-jupyter "0.1.17"]]}
+  ;:jupyter {
+
+  ;  :dependencies [ [clojupyter "0.3.1"]]
+  ;  :plugins [[hellonico/lein-jupyter "0.1.17"]]}
+             :gorilla {:dependencies [[hellonico/gorilla-repl "0.4.1"]]
+                       :plugins [[hellonico/lein-gorilla "0.4.2"]]}}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [seesaw "1.4.5"]
